58bb7360bb03ee3026ac0c1ee832430284e3be89
JoseAnnunziatoNeu <jannunzi@gmail.com>
2016-01-14 16:12:47 -0500

    Added user webservice

--- server.js
+++ server.js
@@ -4,8 +4,20 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.get('/hello', sayHello);
 
+app.get('/user', getAllUsers);
+
 function sayHello(req, res) {
     res.send('hello world 123');
+}
+
+function getAllUsers(req, res) {
+    var users = [
+        {username: "alice", firstName: "Alice", lastName: "Wonrdeland"},
+        {username: "bob", firstName: "Bob", lastName: "Hope"},
+        {username: "charlie", firstName: "Charlie", lastName: "Garcia"}
+    ];
+
+    res.json(users);
 }
 
 app.listen(port, ipaddress);
