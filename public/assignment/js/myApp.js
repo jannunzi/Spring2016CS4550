48badcc1752f99cd91b871425cc94ae48bf86123
JoseAnnunziatoNeu <jannunzi@gmail.com>
2016-01-21 15:27:32 -0500

    Added public Added dashboard experiment Added table experiment

---
+++ public/assignment/js/myApp.js
@@ -0,0 +1,3 @@
+/**
+ * Created by annunziatoj on 1/19/16.
+ */
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/hello', sayHello);
 
 app.get('/user', getAllUsers);
